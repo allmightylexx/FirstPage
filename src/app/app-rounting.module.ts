144342cf9dc7be2649467051c711439021147a96
allmightylexx <allmightylexx@yandex.ru>
2020-08-20 22:31:48 +0300

    add header styles and comp 3(page 3)

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PageOneComponent} from './page-one/page-one.component';
-import {PageTwoComponent} from './page-two/page-two.component'; // CLI imports router
+import {PageTwoComponent} from './page-two/page-two.component';
+import {PageThreeComponent} from './page-three/page-three.component'; // CLI imports router
 
 const routes: Routes = [
   { path: '',   redirectTo: 'page-one', pathMatch: 'full' },
   { path: 'page-one', component: PageOneComponent},
-  { path: 'page-two', component: PageTwoComponent}
+  { path: 'page-two', component: PageTwoComponent},
+  { path: 'page-three', component: PageThreeComponent}
 
 
 ]; // sets up routes constant where you define your routes
